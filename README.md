10776515e1b724348d9c193d846c14706d5a7f6f
Elizabeth Billings <elizabethbillings93@gmail.com>
2022-04-12 17:04:04 -0500

    Made the password appear on page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PasswordGenerator
+After being prompt with questions for the user, the information is looped through to pull a random password displayed on the page. 
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,45 @@
 let genbutton=document.getElementById("GenPassword");  
-let userchoice =[ ]
+let userchoice =["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
 character = ["!","@","#","$","%","^","&","*","(",",",")","-","=","+","/",".",",","?","<",">",";",":"];
 number =[1,2,3,4,5,6,7,8,9];
-letter = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
-
-function writePassword(){
-    var password =generatePassword();
-    var passwordText =document.getElementById("yoursecurepw");
-    passwordText.value =password;
-}
+uppercase=["Q","W", "E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]
 // setting up prompt questions
-function generatePassword (){
 let passwordlength = prompt("How many characters would you like it to be? Needs to be between 8-128")
 let hascharacter = confirm("Do you want special characters in your password?")
 let hasnumber = confirm("Do you want numbers in your password?")
 let hasupper= confirm ("Do you want uppercase letters?")
-genbutton.addEventListener("click",writePassword)
+
+function generatePassword (){
+
 if (passwordlength <= 128 && passwordlength >= 8){
-    console.log(passwordlength + " Number of characters",getRandomLower);
-    function getRandomLower(){
-    return String.fromCharCode(Math.floor(Math.random()* 26) +97);
-}
+    console.log(passwordlength + " Number of characters");
+
 }
 if(hascharacter === true){
     console.log ("Does want special characters.");
-    function getRandomSymbol(){
-    return character[Math.floor(Math.random() * character.length)];
+    userchoice = userchoice.concat(character);
+    console.log(userchoice);
 }
-}
-  
-
 if (hasnumber === true){
-    console.log ("Does want numbers.",getRandomNumber);
-    function getRandomNumber(){
-    return String.fromCharCode[Math.floor(Math.random()* 10) +48];
-}
+    console.log ("Does want numbers.");
+    userchoice = userchoice.concat(number);
+    console.log(userchoice);
 }
 if (hasupper === true){
-    console.log ("Does want uppercase letters.",getRandomUpper);
-    function getRandomUpper(){
-    return String.fromCharCode[Math.floor(Math.random()* 26) +64];
-    }
+    console.log ("Does want uppercase letters.");
+    userchoice = userchoice.concat(uppercase);
+    console.log(userchoice);
 }
-if  (hasupper === false){
-    console.log ("Does not want uppercase.");
+var finalPassword= '';
+ for (let i = 0; i < passwordlength; i++) {
+    var randomNumber = Math.floor(Math.random()* userchoice.length);
+    // console.log(userchoice[randomNumber]);
+    finalPassword+=userchoice[randomNumber];
+
+ }
+ document.getElementById("yoursecurepw").value=finalPassword;
 }
-if (hascharacter === false){
-    console.log ("Does not want special characters.");
-}
-if (hasnumber === false){
-    console.log ("Does not want numbers.");  
-}  
-}
-    generatePassword(); 
-  
+
+
+ generatePassword(); 
+
